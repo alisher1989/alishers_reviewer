79c7567a866ddc9e9a163a63195ad59a1e766be7
alisher <shera.rashiduulu@gmail.com>
2019-11-16 12:50:24 +0200

    Created classes Product and Review, and filled out admin.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,53 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('other', 'Другое'),
+    ('food', 'Еда'),
+    ('clothes', 'Одежда'),
+    ('household', 'Товары для дома'),
+)
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Товар', null=False, blank=False)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name='Категория', null=False, blank=False)
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='описание')
+    photo = models.ImageField(upload_to='product_images', null=True, blank=True, verbose_name='Фото')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+RAITING_CHOICES = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+)
+
+class Order(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL,
+                             verbose_name='Пользователь', related_name='orders')
+    product = models.ForeignKey(Product, related_name='product_order', max_length=100, on_delete=models.CASCADE)
+    order_description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='описание отзыва')
+    rating = models.CharField(max_length=50, choices=RAITING_CHOICES, default=None,
+                                verbose_name='Рейтинг', null=False, blank=False)
+
+    def __str__(self):
+        return str(self.author)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+
+
