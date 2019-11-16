de0eff056c3551c83865dcf3a75005b70be70ae7
alisher <shera.rashiduulu@gmail.com>
2019-11-16 12:51:07 +0200

    installed Pillow and added to requirements.txt

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Product, Order
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'description')
+    list_filter = ('category',)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'order_description', 'rating')
+    list_filter = ('rating',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
